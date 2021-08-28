e3a02a53099b89d3c90191ec016057948b000efa
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2021-08-28 17:35:27 +0100

    wahala done start

--- js/form_validate.js
+++ js/form_validate.js
@@ -9,10 +9,20 @@ function validateData() {
 
 
     // perform data validation
+    let username_error = "fgrr";
+    let phonenumber_error = "";
+    let useremail_error = "";
+
+    if (user_name === "") {
+        username_error = "the user name is required";
+        return false;
+    }
+
+    let error_message = user_name + "\n" + user_email + "\n" + phone_number;
+    document.getElementById('alat').innerHTML = error_message;
 
 
-
-    // print out result
-    var message = "User name is: " + user_name + " \nPhone number is: " + phone_number + " \nUser email is: " + user_email
-    alert(message);
+    // // print out result
+    // var message = "User name is: " + user_name + " \nPhone number is: " + phone_number + " \nUser email is: " + user_email
+    // alert(message);
 }
