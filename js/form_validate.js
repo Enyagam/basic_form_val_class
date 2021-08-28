64af8cbd447482d8ae02efce891ec48fb63699c8
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2021-08-28 17:04:12 +0100

    feat: created the validation funtion

--- js/form_validate.js
+++ js/form_validate.js
@@ -1,1 +1,18 @@
 // function to validate input
+
+function validateData() {
+    // get the input values
+
+    let user_name = document.getElementById('user_name').value;
+    let phone_number = document.getElementById('phone_number').value;
+    let user_email = document.getElementById('user_email').value;
+
+
+    // perform data validation
+
+
+
+    // print out result
+    var message = "User name is: " + user_name + " \nPhone number is: " + phone_number + " \nUser email is: " + user_email
+    alert(message);
+}
